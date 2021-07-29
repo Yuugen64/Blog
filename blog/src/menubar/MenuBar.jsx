fcd30ed9a0504aeec1d99a12ea936cfdb461f512
Yuugen64 <yuugen64@protonmail.com>
2021-07-29 15:36:34 -0500

    [004-0.0.3]:: Rendering a local Image >> Got a local image rendering properly.

--- blog/src/menubar/MenuBar.jsx
+++ blog/src/menubar/MenuBar.jsx
@@ -1,12 +1,42 @@
 import "./menubar.css"
+import oasis from "./oasis.jpg"
 
 export default function MenuBar() {
 
     return (
         <div className="menuBar">
-            <div className="left">left</div>
-            <div className="center">center</div>
-            <div className="right">right</div>
+            <div className="left">
+            <i className="menuMediaIcon" class="fas fa-envelope"></i>
+            <i className="menuMediaIcon" class="fab fa-github"></i>
+            <i className="menuMediaIcon" class="fab fa-pinterest"></i>
+            <i className="menuMediaIcon" class="fab fa-youtube"></i>
+            </div>
+
+
+            <div className="center">
+                <ul className="menuList">
+                    <li className="menuListItem">HOME</li>
+                    <li className="menuListItem">ABOUT</li>
+                    <li className="menuListItem">CONTACT</li>
+                    <li className="menuListItem">WRITE</li>
+                    <li className="menuListItem">LOGOUT</li>
+                </ul>
+            </div>
+
+
+            <div className="right">
+                <img
+                className="menuImage"
+                src={oasis}
+                alt=""
+                />
+
+                <p>Image goes here</p>
+
+                <i className="menuSearchIcon" class="fas fa-search"></i>
+            </div>
+
+
         </div>
     )
 
