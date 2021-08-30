a61715cb94c359aedcf7300badc0d329442875ea
Yuugen64 <yuugen64@protonmail.com>
2021-08-30 15:31:55 -0500

    [021-0.1.3]:: REGISTER & LOGIN routes >> Spent a few weeks trying to better understand routing. Managed to get a Register and Login routes correctly sending data through postman to MongoDB Atlas. Also implemented hasing to obfuscate passwords in the database and took care to make sure that the passwords were filtered out in JSON responses.

--- blog/api/routes/auth.js
+++ blog/api/routes/auth.js
@@ -1,14 +1,22 @@
 const router = require("express").Router();
-
 const User = require("../models/User");
+
+const bcrypt = require('bcrypt');
 
 //REGISTER
 router.post("/register", async(req,res)=>{
     try{
+
+        //Salt for the hash
+        const salt = await bcrypt.genSalt(10);
+        //Getting the entered password and hashing it with our salt, storing as 'hashedPass'
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: hashedPass
         })
 
         const user = await newUser.save();
@@ -20,7 +28,20 @@ router.post("/register", async(req,res)=>{
 
 
 //LOGIN
+router.post("/login", async (req,res)=>{
+    try {
+        const user = await User.findOne({username: req.body.username});
+        !user && res.status(400).json("Wrong Credentials!");
 
+        const validated = await bcrypt.compare(req.body.password, user.password);
+        !validated && res.status(400).json("Wrong Credentials!");
+
+        const {password, ...others} = user._doc;
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 module.exports = router;
